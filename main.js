7c4d1a65690a96e39494510db26b2c96c34a503a
Henrique Fernandez <rickfernandez03@gmail.com>
2024-05-18 21:00:44 -0300

    adjust on home animation on mobile and animation code refactor

--- main.js
+++ main.js
@@ -1,93 +1,88 @@
-window.addEventListener('wheel', onWheel)
-window.addEventListener('scroll', onScroll)
+window.addEventListener('wheel', onWheel);
+window.addEventListener('scroll', onScroll);
 
-const navigation = document.querySelector('#navigation')
-const header = document.querySelector('.fixed-menu')
-const btnMenu = document.querySelector('.btn-menu')
-const cube = document.querySelector('.cube')
-const rotation = 'rotateX(90deg)'
-const zoomOut = 'scale(0.3)'
-const zoomIn = 'scale(1)'
-let currentFace = 'front'
-let animationActive = false
-let menuIsOpen = false
+const navigation = document.querySelector('#navigation');
+const header = document.querySelector('.fixed-menu');
+const btnMenu = document.querySelector('.btn-menu');
+const scrollBtn = document.querySelector('.scroll-btn');
+const cube = document.querySelector('.cube');
+const rotation = 'rotateX(90deg)';
+const zoomOut = 'scale(0.3)';
+const zoomIn = 'scale(1)';
 
-onWheel()
-onScroll()
+let currentFace = 'front';
+let animationActive = false;
+let menuIsOpen = false;
+let scrollFromClick = false;
+
+onWheel();
+onScroll();
 
 function onWheel() {
-  onAnimation()
+  onAnimation();
 }
 
 function onScroll() {
-  styledHeader()
+  styledHeader();
+}
+
+function onScrollFromClick() {
+  scrollFromClick = true;
+  onAnimation();
 }
 
 function onAnimation() {
   if (!animationActive && !navigation.classList.contains('opened')) {
-    const backFace = document.querySelector('.back')
+    const backFace = document.querySelector('.back');
 
-    if (currentFace === 'front' && event.deltaY > 0) {
-      startAnimationToBack()
+    if (currentFace === 'front' && (event.deltaY > 0 || scrollFromClick)) {
+      startAnimation('180deg', 'back');
     } else if (
       currentFace === 'back' &&
       event.deltaY < 0 &&
       backFace.scrollTop === 0
     ) {
-      startAnimationToFront()
+      startAnimation('0', 'front');
     }
   }
 }
 
-function startAnimationToBack() {
-  animationActive = true
+function startAnimation(rotateValue, toFace) {
+  animationActive = true;
 
-  cube.style.transform = zoomOut + ' ' + rotation
-
-  setTimeout(() => {
-    cube.style.transform = zoomIn + ' ' + 'rotateX(180deg)'
-    currentFace = 'back'
-    animationActive = false
-    styledHeader()
-  }, 1300)
-}
-
-function startAnimationToFront() {
-  animationActive = true
-
-  cube.style.transform = zoomOut + ' ' + rotation
+  cube.style.transform = zoomOut + ' ' + rotation;
 
   setTimeout(() => {
-    cube.style.transform = zoomIn + ' ' + 'rotateX(0)'
-    currentFace = 'front'
-    animationActive = false
-    styledHeader()
+    cube.style.transform = zoomIn + ' ' + `rotateX(${rotateValue})`;
+    currentFace = toFace;
+    animationActive = false;
+    scrollFromClick = false;
+    styledHeader();
   }, 1300)
 }
 
 function onMenu() {
   if (btnMenu.classList.contains('opened')) {
-    navigation.classList.remove('opened')
-    btnMenu.classList.remove('opened')
-    header.classList.add('styled')
+    navigation.classList.remove('opened');
+    btnMenu.classList.remove('opened');
+    header.classList.add('styled');
   } else {
-    header.classList.remove('styled')
-    navigation.classList.add('opened')
-    btnMenu.classList.add('opened')
+    header.classList.remove('styled');
+    navigation.classList.add('opened');
+    btnMenu.classList.add('opened');
   }
 
-  document.body.classList.add('menu-expanded')
+  document.body.classList.add('menu-expanded');
 }
 
 function styledHeader() {
-  console.log('chamou')
   if (scrollY > 0 || currentFace !== 'front') {
     if (navigation.classList.contains('opened')) {
-      header.classList.remove('styled')
+      header.classList.remove('styled');
     } else {
-      header.classList.add('styled')
+      header.classList.add('styled');
     }
   } else {
-    header.classList.remove('styled')
+    header.classList.remove('styled');
   }
 }
