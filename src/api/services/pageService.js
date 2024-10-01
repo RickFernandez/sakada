8519c03e0c30064cda3fbe6ca6c6573556f4d15e
Henrique Fernandez <rickfernandez03@gmail.com>
2024-10-01 00:22:11 -0300

    feat: loading projects and filters dinamicaly to project-pages

--- src/api/services/pageService.js
+++ src/api/services/pageService.js
@@ -1,4 +1,5 @@
 import API_ROUTES from '../apiConfig.js';
+import initializeProjectFilters from '../../../scripts/projects.js';
 
 const pageService = {
   currentPageUrl: '',
@@ -14,11 +15,17 @@ const pageService = {
         const pageData = data.data[0]?.attributes;
         if (pageData) {
           this.updatePageHeader(pageData);
+          if (url.includes('projects')) {
+            this.updateProjectsPage(pageData);
+          }
         }
       })
       .catch(error => console.error('Erro ao carregar os dados da página:', error))
       .finally(() => {
         this.hideLoader();
+        if (url.includes('projects')) {
+          initializeProjectFilters();
+        }
       });
   },
 
@@ -41,12 +48,80 @@ const pageService = {
       titleElement.textContent = title;
     }
 
-    pageHeaderElement.style.backgroundImage = `url(${pageData.pageHeader.backgroundMedia.data?.attributes.url})`;
+    pageHeaderElement.style.backgroundImage = `url(${pageData.pageHeader.backgroundMedia.data?.attributes.url || ''})`;
     pageHeaderElement.style.backgroundRepeat = 'no-repeat';
     pageHeaderElement.style.backgroundPosition = 'center';
     pageHeaderElement.style.backgroundSize = 'cover';
 
     document.getElementById('page-content').style.display = 'block';
+  },
+
+  updateProjectsPage(pageData) {
+    this.generateProjectFilters(pageData.project_filters);
+    this.generateProjects(pageData.projects);
+  },
+
+  generateProjectFilters(filters) {
+    const filterContainer = document.querySelector('.filter');
+    const filterMobileContainer = document.querySelector('.filter-mobile .filter-options');
+
+    filterContainer.innerHTML = '';
+    filterMobileContainer.innerHTML = '<div class="filter-header"><span>Filter</span><img class="close-filter-btn" src="/assets/icons/close-menu-icon.svg" alt="" /></div>';
+
+    this.createFilterButton(filterContainer, 'all', 'All');
+    this.createFilterButton(filterMobileContainer, 'all', 'All');
+
+    filters.data.forEach(filter => {
+      const filterName = filter.attributes.name.toLowerCase().replace(/ /g, '-');
+      this.createFilterButton(filterContainer, filterName, filter.attributes.name);
+      this.createFilterButton(filterMobileContainer, filterName, filter.attributes.name);
+    });
+  },
+
+  createFilterButton(container, filterName, displayName) {
+    const button = document.createElement('button');
+    button.className = container.classList.contains('filter-options') ? 'btn-mobile' : 'btn';
+    button.setAttribute('data-filter', filterName);
+    button.textContent = displayName;
+    container.appendChild(button);
+  },
+
+  generateProjects(projects) {
+    const projectsContainer = document.querySelector('.projects');
+
+    projectsContainer.innerHTML = '';
+
+    projects.data.forEach(project => {
+      const projectElement = document.createElement('div');
+      projectElement.className = 'project';
+
+      const filters = project.attributes.project_filters.data.map(filter =>
+        filter.attributes.name.toLowerCase().replace(/ /g, '-')
+      );
+      projectElement.classList.add(...filters);
+
+      const imgElement = document.createElement('img');
+      imgElement.src = project.attributes.wallpaperMedia.data?.attributes.url || '';
+      imgElement.alt = '';
+
+      const viewProjectElement = document.createElement('div');
+      viewProjectElement.className = 'view-project';
+
+      const linkElement = document.createElement('a');
+      linkElement.href = project.attributes.referenceUrl || '#';
+      linkElement.textContent = 'Ver Projeto';
+
+      const arrowImgElement = document.createElement('img');
+      arrowImgElement.src = '../assets/icons/arrow-right-icon.svg';
+      arrowImgElement.alt = '';
+
+      viewProjectElement.appendChild(linkElement);
+      viewProjectElement.appendChild(arrowImgElement);
+      projectElement.appendChild(imgElement);
+      projectElement.appendChild(viewProjectElement);
+
+      projectsContainer.appendChild(projectElement);
+    });
   },
 
   showLoader() {
