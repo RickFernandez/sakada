50b93cfc7b0f8f4ca7df7b8154881c4b70d3346e
Henrique Fernandez <rickfernandez03@gmail.com>
2024-10-01 00:21:37 -0300

    refactor: editing page request url parameters

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -5,7 +5,7 @@ const API_URL = import.meta.env.VITE_API_BASE_URL;
 const API_ROUTES = {
   team: `${API_URL}/team?populate[employees][populate][picture][fields][0]=url`,
   categories: `${API_URL}/categories?populate[projects][fields][0]=referenceUrl&populate[projects][fields][1]=projectName&populate[projectFilters][fields][0]=name`,
-  pages: `${API_URL}/pages?populate[pageHeader][populate][backgroundMedia][fields][0]=url`,
+  pages: `${API_URL}/pages?populate[pageHeader][populate][backgroundMedia][fields][0]=url&populate[projects][fields][0]=referenceUrl&populate[projects][populate][wallpaperMedia][fields][0]=url&populate[projects][populate][project_filters][fields][0]=name&populate[project_filters][fields][0]=name&filters[urlReference]=digital-projects`,
   projects: `${API_URL}/projects?populate[presentationMedia][fields][0]=url&populate[challengeMedias][fields][0]=url&populate[solutionMedia][fields][0]=url&populate[testimonyMedias][fields][0]=url&populate[header][populate][backgroundMedia][fields][0]=url`,
   projectFilters: `${API_URL}/project-filters?populate[projects][fields][0]=referenceUrl&populate[projects][fields][1]=projectName&populate[categories][fields][0]=name`
 }
