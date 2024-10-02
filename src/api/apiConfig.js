d85153ddaf8d60dfb899229677f2444be13aa4fc
Henrique Fernandez <rickfernandez03@gmail.com>
2024-10-02 00:06:04 -0300

    refactor: updating pages-api-route to return more properties values

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,13 +1,12 @@
-// Lê a variável de ambiente para definir a URL base
 const API_URL = import.meta.env.VITE_API_BASE_URL;
 
-// Define as rotas das APIs
 const API_ROUTES = {
   team: `${API_URL}/team?populate[employees][populate][picture][fields][0]=url`,
   categories: `${API_URL}/categories?populate[projects][fields][0]=referenceUrl&populate[projects][fields][1]=projectName&populate[projectFilters][fields][0]=name`,
-  pages: `${API_URL}/pages?populate[pageHeader][populate][backgroundMedia][fields][0]=url&populate[projects][fields][0]=referenceUrl&populate[projects][populate][wallpaperMedia][fields][0]=url&populate[projects][populate][project_filters][fields][0]=name&populate[project_filters][fields][0]=name&filters[urlReference]=digital-projects`,
+  pages: `${API_URL}/pages?populate[pageHeader][populate][backgroundMedia][fields][0]=ur&populate[projects][fields][0]=referenceUrl&populate[projects][populate][wallpaperMedia][fields][0]=url&populate[projects][populate][project_filters][fields][0]=name&populate[project_filters][fields][0]=name&populate[team][populate][employee][fields][0]=name&populate[team][populate][employee][fields][1]=description&populate[team][populate][employee][populate][picture][fields][0]=url&populate[clients][populate][client][populate][logo][fields][0]=url`,
   projects: `${API_URL}/projects?populate[presentationMedia][fields][0]=url&populate[challengeMedias][fields][0]=url&populate[solutionMedia][fields][0]=url&populate[testimonyMedias][fields][0]=url&populate[header][populate][backgroundMedia][fields][0]=url`,
-  projectFilters: `${API_URL}/project-filters?populate[projects][fields][0]=referenceUrl&populate[projects][fields][1]=projectName&populate[categories][fields][0]=name`
+  projectFilters: `${API_URL}/project-filters?populate[projects][fields][0]=referenceUrl&populate[projects][fields][1]=projectName&populate[categories][fields][0]=name`,
+  client: `${API_URL}/client?populate[client][populate][logo][fields][0]=url`
 }
 
 export default API_ROUTES;
