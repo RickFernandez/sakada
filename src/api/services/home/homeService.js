6e7fd3cf64f1009410095713c9985f3a1c1ff7a0
Henrique Fernandez <rickfernandez03@gmail.com>
2024-10-08 21:36:07 -0300

    refactor: removing console.log and adjusting project page url

--- src/api/services/home/homeService.js
+++ src/api/services/home/homeService.js
@@ -7,7 +7,7 @@ const homeService = {
 
   setProjects(projects) {
     const homeProjectsSection = document.querySelector('.home-projects');
-  
+
     projects.data.forEach(project => {
       const projectElement = document.createElement('a');
       projectElement.className = 'project-presentation text';
@@ -16,53 +16,52 @@ const homeService = {
       projectElement.style.backgroundRepeat = 'no-repeat';
       projectElement.style.backgroundPosition = 'center';
       projectElement.style.backgroundSize = 'cover';
-  
+
       projectElement.addEventListener('click', (event) => {
         event.preventDefault();
         const referringPage = '/';
-        
-        window.location.href = `/pages/project?projectName=${project.attributes.referenceUrl}&referrer=${referringPage}`;
+
+        window.location.href = `src/pages/project/project?projectName=${project.attributes.referenceUrl}&referrer=${referringPage}`;
       });
-  
+
       homeProjectsSection.appendChild(projectElement);
     });
   },
 
   setTeam(employees) {
-    console.log(employees);
     const teamSection = document.querySelector('.team .employees');
-  
+
     employees.forEach(employee => {
       const employeeDiv = document.createElement('div');
       employeeDiv.classList.add('employee');
-  
+
       const employeeImg = document.createElement('img');
       employeeImg.src = employee.picture.data.attributes.url;
       employeeImg.alt = employee.name;
-  
+
       const textsDiv = document.createElement('div');
       textsDiv.classList.add('texts');
-  
+
       const employeeName = document.createElement('h6');
       employeeName.classList.add('employee-name');
       employeeName.textContent = employee.name;
-  
+
       const employeeDescription = document.createElement('p');
       employeeDescription.classList.add('text');
       employeeDescription.textContent = employee.description;
-  
+
       textsDiv.appendChild(employeeName);
       textsDiv.appendChild(employeeDescription);
       employeeDiv.appendChild(employeeImg);
       employeeDiv.appendChild(textsDiv);
-  
+
       teamSection.appendChild(employeeDiv);
     });
   },
-  
+
   setClients(clients) {
     const clientsBox = document.querySelector('.clients-box');
-  
+
     clients.forEach(client => {
       const imgElement = document.createElement('img');
       imgElement.src = client.logo.data[0].attributes.url;
